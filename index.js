1586a6f878d3df0f6a78dff2d77fd0a178514bd8
Grant Rigdon <grant_rigdon@ymail.com>
2019-04-17 19:26:48 -0400

    added gallery

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ $(function () { // wait for document ready
   var controller = new ScrollMagic.Controller();
   // define movement of panels
   var wipeAnimation = new TimelineMax()
-      .fromTo("div.panel.second", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
-      .fromTo("div.panel.third",    1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
-      .fromTo("div.panel.fourth", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}) // in from top
-      .fromTo("div.panel.fifth", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone}); // in from bottom
+      .fromTo("div.panel.second", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})  // in from left
+      .fromTo("div.panel.third",    1, {y:  "100%"}, {y: "0%", ease: Linear.easeNone})  // in from right
+      // .fromTo("div.panel.fourth", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}) // in from top
+      // .fromTo("div.panel.fifth", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone}); // in from bottom
       // create scene to pin and link animation
   new ScrollMagic.Scene({
       triggerElement: "#pinContainer",
@@ -17,3 +17,4 @@ $(function () { // wait for document ready
   .setTween(wipeAnimation)
   .addTo(controller);
 });
+

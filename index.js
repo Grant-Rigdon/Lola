6c66d56fbc458f0088018a9b5e87a54a65ace2e0
Grant Rigdon <grant_rigdon@ymail.com>
2019-04-18 22:28:27 -0400

    mobile?

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(function () { // wait for document ready
   // define movement of panels
   var wipeAnimation = new TimelineMax()
       .fromTo("div.panel.second", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})  // in from left
-      .fromTo("div.panel.third",    1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
+      .fromTo("div.panel.third",    1, {y:  "100%"}, {y: "0%", ease: Linear.easeNone})  // in from right
       // .fromTo("div.panel.fourth", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}) // in from top
       // .fromTo("div.panel.fifth", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone}); // in from bottom
       // create scene to pin and link animation
